bdcbeceed7213256fa951c399d994d0d5681064c
John Wagner <john.wagnerm@gmail.com>
2024-04-26 00:59:47 -0500

    prep 5 assignments

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        void DisplayWelcome()
+        {
+            Console.Write("Welcome to the Program!");
+        }
+        String PromptUserName()
+        {
+            Console.Write("Please, enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        int PromptUserNumber()
+        {
+            Console.Write("Please, enter your favorite number: ");
+            string numberInput = Console.ReadLine();
+            int number = int.Parse(numberInput);
+            return number;
+        }
+        int SquareNumber(int numberToSquare)
+        {
+            int number = numberToSquare * numberToSquare;
+            return number;
+        }
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        Console.WriteLine($"{name}, the square of your number is {SquareNumber(number)}");
     }
 }
