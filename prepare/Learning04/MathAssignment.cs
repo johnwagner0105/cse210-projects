4c8af9b3b03f1642ea339a53853383a1e99c6263
John Wagner <john.wagnerm@gmail.com>
2024-05-19 00:49:37 -0500

    learning04 activity

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,15 @@
+public class MathAssignment : Assignment
+{
+    private string _textbookSection, _problems;
+
+    public MathAssignment(string textbookSection, string problems, string studentName, string topic) : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment01 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment01.GetSummary());
+
+        MathAssignment assignment02 = new MathAssignment("7.3", "8-19", "Roberto Rodriguez", "Fractions");
+        Console.WriteLine(assignment02.GetSummary());
+        Console.WriteLine(assignment02.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
